b4e77ac158709a496321bbab835094e6dcf01d58
Tiger Abrodi <tigerabrodi@gmail.com>
2024-01-28 15:49:56 +0100

    Refactor paymentProcessingHandler.js to process multiple orders

--- aws_lambdas/paymentProcessingHandler.js
+++ aws_lambdas/paymentProcessingHandler.js
@@ -5,47 +5,33 @@ const sns = new AWS.SNS({ apiVersion: "2010-03-31" });
 const SQS_QUEUE_URL = process.env.SQS_QUEUE_URL;
 const SNS_TOPIC_ARN = process.env.SNS_TOPIC_ARN;
 
-exports.handler = async () => {
+exports.handler = async (event) => {
   try {
-    // Poll the order_queue
-    const data = await sqs
-      .receiveMessage({
-        QueueUrl: SQS_QUEUE_URL,
-        MaxNumberOfMessages: 1,
-      })
-      .promise();
-
-    if (!data.Messages) {
+    if (!event.Records) {
       console.log("No new orders to process");
       return;
     }
 
-    const order = JSON.parse(data.Messages[0].Body);
+    for (const record of event.Records) {
+      const order = JSON.parse(record.body);
 
-    // Simulate payment processing
-    console.log(`Processing payment for order: ${order.transactionId}`);
-    // Add your payment processing logic here
+      // Simulate payment processing
+      console.log(`Processing payment for order: ${order.transactionId}`);
+      // Add your payment processing logic here
 
-    // On successful payment, push a message to payment_status_topic
-    const params = {
-      Message: `Payment for order ${order.transactionId} has been processed successfully`,
-      TopicArn: SNS_TOPIC_ARN,
-    };
+      // On successful payment, push a message to payment_status_topic
+      const params = {
+        Message: `Payment for order ${order.transactionId} has been processed successfully`,
+        TopicArn: SNS_TOPIC_ARN,
+      };
 
-    await sns.publish(params).promise();
-    console.log(
-      `Payment status for order ${order.transactionId} published to SNS topic`
-    );
+      await sns.publish(params).promise();
+      console.log(
+        `Payment status for order ${order.transactionId} published to SNS topic`
+      );
 
-    // Delete the processed message from the queue
-    await sqs
-      .deleteMessage({
-        QueueUrl: SQS_QUEUE_URL,
-        ReceiptHandle: data.Messages[0].ReceiptHandle,
-      })
-      .promise();
-
-    console.log(`Order ${order.transactionId} removed from the queue`);
+      console.log(`Order ${order.transactionId} processed successfully`);
+    }
   } catch (error) {
     console.error(`Error processing payment: ${error}`);
   }
