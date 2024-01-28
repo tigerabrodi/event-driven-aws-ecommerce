706231e4b5af504456133207908c719e18efded1
Tiger Abrodi <tigerabrodi@gmail.com>
2024-01-28 07:54:57 +0100

    Add AWS Lambdas code and configuration files

---
+++ aws_lambdas/orderManagementHandler.js
@@ -0,0 +1,29 @@
+const AWS = require("aws-sdk");
+const sqs = new AWS.SQS({ apiVersion: "2012-11-05" });
+const QUEUE_URL = process.env.QUEUE_URL;
+
+exports.handler = async (event) => {
+  try {
+    console.log("Received event:", JSON.stringify(event, null, 2));
+
+    // Validate the incoming order data
+    if (!event.transactionId || !event.customerId) {
+      console.error("Invalid order data");
+      return { statusCode: 400, body: "Invalid order data" };
+    }
+
+    const params = {
+      MessageBody: JSON.stringify(event),
+      QueueUrl: QUEUE_URL,
+    };
+
+    // Send the message to SQS
+    await sqs.sendMessage(params).promise();
+    console.log(`Order message sent to SQS: ${event.transactionId}`);
+
+    return { statusCode: 200, body: "Order processed successfully" };
+  } catch (error) {
+    console.error(`Error processing order: ${error}`);
+    return { statusCode: 500, body: "Error processing order" };
+  }
+};
